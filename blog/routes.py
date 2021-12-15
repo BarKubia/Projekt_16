643674179c6dcae100b8d1de841eaf007460d04e
Bartosz Kubiak <barkubia1@gmail.com>
2021-12-15 20:16:05 +0100

    End refactoring

--- blog/routes.py
+++ blog/routes.py
@@ -4,6 +4,34 @@ from faker import Faker
 from blog.models import Entry, db
 from blog.forms import EntryForm
 
+def edit_create_entry(function, entry_id):
+   errors = None
+   if function:
+      form = EntryForm()
+      entry = Entry(
+         title=form.title.data,
+         body=form.body.data,
+         is_published=form.is_published.data
+         )
+            
+   else:
+      entry = Entry.query.filter_by(id=entry_id).first_or_404()
+      form = EntryForm(obj=entry)
+   
+   if request.method == 'POST':
+      if form.validate_on_submit():
+         if function:
+            db.session.add(entry)
+         else:
+            form.populate_obj(entry)
+
+         db.session.commit()
+      else:
+         errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
+
+
+
 
 
 
@@ -29,32 +57,9 @@ def generate_entries(how_many=10):
 
 @app.route("/new-post/", methods=["GET", "POST"])
 def create_entry():
-   form = EntryForm()
-   errors = None
-   if request.method == 'POST':
-      if form.validate_on_submit():
-          entry = Entry(
-             title=form.title.data,
-             body=form.body.data,
-             is_published=form.is_published.data
-             )
-          db.session.add(entry)
-          db.session.commit()
-      else:
-          errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
-
+   return edit_create_entry(function=True, entry_id=999)
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
 def edit_entry(entry_id):
-   entry = Entry.query.filter_by(id=entry_id).first_or_404()
-   form = EntryForm(obj=entry)
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           form.populate_obj(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
+   return edit_create_entry(function=False, entry_id=entry_id)
